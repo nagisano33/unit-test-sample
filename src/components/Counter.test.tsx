8069e3649cdf4896c899faaa2ffcd668198793db
nagisano33 <vita.stars2@gmail.com>
2023-05-22 01:54:40 +0900

    Add AAA パターンで書いてみた単体テスト

---
+++ src/components/Counter.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Counter } from "./Counter";
+
+test("initialCount に 1 を渡すと「現在のカウント:1」と表示されるか", () => {
+  // arrange
+  render(<Counter initialCount={1} />);
+
+  // act (なし)
+
+  // assetion
+  const element = screen.getByText("現在のカウント:1");
+  expect(element).toBeInTheDocument();
+});
+
+test("「カウントアップ」押下で現在のカウントが+1されるか", () => {
+  // arrange
+  render(<Counter initialCount={0} />);
+
+  // act (なし)
+  const button = screen.getByRole("button");
+  fireEvent.click(button);
+
+  // assetion
+  const element = screen.getByText("現在のカウント:1");
+  expect(element).toBeInTheDocument();
+});
